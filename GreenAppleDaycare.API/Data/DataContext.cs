9bb593440e63926851c863b28a12b93fcd2c3d63
Nychols <enichols@glassfabtempering.com>
2020-08-13 12:33:54 -0700

    Initial Commit

---
+++ GreenAppleDaycare.API/Data/DataContext.cs
@@ -0,0 +1,11 @@
+using GreenAppleDaycare.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace GreenAppleDaycare.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base (options){}
+        public DbSet<Values> values { get; set; }
+    }
+}
